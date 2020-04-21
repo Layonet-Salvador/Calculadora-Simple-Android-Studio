744a2f1042c00c5b7adc2ea04cb12e713788b64a
Layonet Salvador <41496055+Layonet-Salvador@users.noreply.github.com>
2020-04-21 18:31:37 -0500

    Calculadora 21-04-22 botones = Suma, Resta, Limpiar

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1;
+    EditText et2;
+    TextView res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1 =(EditText)findViewById(R.id.valor1);
+        et2 =(EditText)findViewById(R.id.valor2);
+        res =(TextView)findViewById(R.id.resutado);
+    }
+
+    public void suma(View view)
+    {
+        String val1= et1.getText().toString();
+        String val2= et2.getText().toString();
+        int valfinal1 = Integer.parseInt(val1);
+        int valfinal2 = Integer.parseInt(val2);
+        int suma = valfinal1 + valfinal2;
+
+        String resultado = String.valueOf(suma);
+        res.setText(resultado);
+    }
+    public void resta(View view)
+    {
+        String val1= et1.getText().toString();
+        String val2= et2.getText().toString();
+        int valfinal1 = Integer.parseInt(val1);
+        int valfinal2 = Integer.parseInt(val2);
+        int resta = valfinal1 - valfinal2;
+
+        String resultado = String.valueOf(resta);
+        res.setText(resultado);
+    }
+    public void limpiar(View view)
+    {
+    et1.setText("");
+    et2.setText("");
+    res.setText("");
+    }
+}
