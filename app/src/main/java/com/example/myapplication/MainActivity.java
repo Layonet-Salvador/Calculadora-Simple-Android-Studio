6716278278dcd1f96d08d508212f06e59984a244
Layonet Salvador <41496055+Layonet-Salvador@users.noreply.github.com>
2020-04-28 16:37:20 -0500

    Modificaciones en diseño y mensajes emergentes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et1 =(EditText)findViewById(R.id.valor1);
         et2 =(EditText)findViewById(R.id.valor2);
-        res =(TextView)findViewById(R.id.resutado);
+        res =(TextView)findViewById(R.id.resultado);
     }
 
     public void suma(View view)
     {
-        String val1= et1.getText().toString();
-        String val2= et2.getText().toString();
-        int valfinal1 = Integer.parseInt(val1);
-        int valfinal2 = Integer.parseInt(val2);
-        int suma = valfinal1 + valfinal2;
+    if(et1.getText().toString().equals("") || et2.getText().toString().equals("")  )
+        {
+            et1.setError("Falta numero");
+            Toast.makeText(this,"Ingresa el valor que falta",Toast.LENGTH_LONG).show();
+        }
+        else {
 
-        String resultado = String.valueOf(suma);
-        res.setText(resultado);
+            String val1 = et1.getText().toString();
+            String val2 = et2.getText().toString();
+            int valfinal1 = Integer.parseInt(val1);
+            int valfinal2 = Integer.parseInt(val2);
+            int suma = valfinal1 + valfinal2;
+
+            String resultado = String.valueOf(suma);
+            res.setText(resultado);
+        }
     }
     public void resta(View view)
     {
